5505809c13617c402a2eb75bd16bfbd50280a571
Prakash <psurya693@gmail.com>
2022-09-11 21:39:19 +0530

    Proxy added in package.json

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "/api";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
@@ -11,7 +11,9 @@ const BASE_URL = "http://localhost:5000/api/";
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL,
+	headers: { "Content-type": "application/json" },
 });
+//
 
 // export const userRequest = axios.create({
 // 	baseURL: BASE_URL,
