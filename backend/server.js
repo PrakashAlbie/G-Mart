dacdd57eb0f2ee1cf54cb6a3c0c9adb1ba7bee00
Prakash <psurya693@gmail.com>
2022-09-11 17:24:12 +0530

    Procfile named

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
@@ -10,6 +9,7 @@ const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 const path = require("path");
+const app = express();
 
 dotenv.config();
 
